b031255306152f1e715de23fe8a709d8330a9540
Nandinski <amandio.faustino@kaust.edu.sa>
2024-05-19 10:34:58 +0300

    colext_config code path defaults to path to config

--- src/colext/experiments/experiment_dispatcher.py
+++ src/colext/experiments/experiment_dispatcher.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import yaml
 import sys
 import logging
@@ -30,21 +31,40 @@ def read_config(config_file):
         print(f"Error parsing configuration file: {yaml_err}")
         sys.exit(1)
 
+    if "devices" not in config_dict:
+        print("Please specify at least one device to act as a client")
+        sys.exit(1)
+
+    if "path" not in config_dict["code"]:
+        default_path = os.path.dirname(os.path.realpath(config_file))
+        config_dict["code"]["path"] = default_path
+        print(f"Could not find 'code.path' in config file. Assuming {default_path}")
+
+    if "python_version" not in config_dict["code"]:
+        default_py_version = "3.8"
+        config_dict["code"]["python_version"] = default_py_version
+        print(f"Could not find 'code.python_version' in config file. Assuming {default_py_version}")
+
+    # Validate python version
+    valid_python_versions = ["3.8", "3.10"]
+    if config_dict["code"]["python_version"] not in valid_python_versions:
+        print(f"code.python_version can  only be set to {valid_python_versions}")
+        sys.exit(1)
+
     # TODO Support specifying device type + count
     config_dict["client_types_to_generate"] = []
     n_clients = 0
+    # count n_clients
     for dev in config_dict["devices"]:
         config_dict["client_types_to_generate"].extend([dev["device_type"]]*dev["count"])
         n_clients += dev["count"]
-
     config_dict["n_clients"] = n_clients
 
-    # Update config based on monitoring defaults
+    # Apply default monitoring details
     monitoring_defaults = {"live_metrics": True, "push_interval": 10, "scrapping_interval": 0.3} # intervals are in seconds
     config_dict["monitoring"] = {**monitoring_defaults, **config_dict.get("monitoring", {})}
 
     print("CoLExT configuration read successfully")
-
     return config_dict
 
 DASHBOARD_URL = ("http://localhost:3000/d/c9b9dcd9-9304-47d7-8dd2-92b8529725c8/colext-dashboard?"
