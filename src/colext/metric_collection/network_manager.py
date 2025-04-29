f1cef5938b223fa90bb121c41077b3f8eb4f2597
Abdullah Alamoudi <abdullah.alamoudi.1@kaust.edu.sa>
2025-04-29 17:30:18 +0300

    silly error fixed

--- src/colext/metric_collection/network_manager.py
+++ src/colext/metric_collection/network_manager.py
@@ -43,10 +43,10 @@ class NetworkGenerator:
         log.debug(f"Network generator created with file {self.file} and type {self.type}")
         if self.filetype == "json":
             self.json_parser()
-            self.generator = self.MakeGenerator()
+            self.MakeGenerator()
             log.debug(f"Network generator with json : {self.generator}")
         elif self.filetype == "script":
-            self.generator = self.MakeGenerator()
+            self.MakeGenerator()
             log.debug(f"Network generator with script : {self.generator}")
   
     def json_parser(self):
