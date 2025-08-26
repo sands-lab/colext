4c186e9356eb34d594d9b17c473548a0309cabca
Nandinski <amandio.faustino@kaust.edu.sa>
2025-08-26 16:22:23 +0300

    Remove trailing slash from datasets path

--- src/colext/common/vars.py
+++ src/colext/common/vars.py
@@ -1,6 +1,6 @@
 REGISTRY="colext:5000"
 
-STD_DATASETS_PATH="/colext/datasets/"
-PYTORCH_DATASETS_PATH="/colext/pytorch_datasets/"
+STD_DATASETS_PATH="/colext/datasets"
+PYTORCH_DATASETS_PATH="/colext/pytorch_datasets"
 
 SMART_PLUG_HOST_MAP_FILE = "/colext/smart_plug_host_map.json"
